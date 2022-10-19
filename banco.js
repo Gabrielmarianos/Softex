f63ce85ad7e0f966e91f1782c1a333f9fedce68a
Gabrielmarianos <107280530+Gabrielmarianos@users.noreply.github.com>
2022-10-19 16:34:17 -0300

    Create banco.js

---
+++ banco.js
@@ -0,0 +1,31 @@
+var banco = {
+  conta: "35.219-14",
+  saldo: 500,
+  tipo: "corrente",
+  agencia: "321-0",
+
+  getSaldo: function () {
+    console.log("O saldo da conta é: R$", banco.saldo);
+  },
+
+  getDeposito: function (deposito) {
+    banco.saldo = banco.saldo + deposito;
+    console.log("Saldo após o deposito: R$", banco.saldo);
+  },
+
+  getSaque: function (saque) {
+    banco.saldo = banco.saldo - saque;
+    console.log("Saldo após o saque: R$", banco.saldo);
+  },
+
+  getNumero_conta: function () {
+    console.log("O número da conta é: ", banco.conta);
+  },
+};
+
+banco.getSaldo();
+console.log("Realizando um deposito");
+banco.getDeposito(300);
+console.log("Realizando um saque");
+banco.getSaque(250);
+banco.getNumero_conta();
